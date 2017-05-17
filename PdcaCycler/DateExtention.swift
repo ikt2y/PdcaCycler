c9de1279b86a77956a5934280c180efede92f1e4
sel <ikd618@gmail.com>
2017-05-18 01:25:18 +0900

    Date型String型間のコンバーダー追加

---
+++ PdcaCycler/DateExtention.swift
@@ -0,0 +1,32 @@
+//
+//  DateExtention.swift
+//  PdcaCycler
+//
+//  Created by sel on 2017/05/18.
+//  Copyright © 2017年 sel. All rights reserved.
+//
+
+import UIKit
+
+extension Date {
+    
+    func dateToString() -> String {
+        let formatter: DateFormatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ja_JP")
+        formatter.dateFormat = "yyyy/MM/dd"
+        return formatter.string(from: self)
+    }
+
+    
+}
+
+extension String {
+    
+    func stringToDate() -> Date {
+        let formatter: DateFormatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ja_JP")
+        formatter.dateFormat = "yyyy/MM/dd"
+        return formatter.date(from: self)!
+    }
+    
+}
