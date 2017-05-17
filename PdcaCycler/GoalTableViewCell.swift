cc25f66bbb139d2ecc72406b2c67653c01501726
sel <ikd618@gmail.com>
2017-05-17 20:26:08 +0900

    GoalTableViewの作成

---
+++ PdcaCycler/GoalTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GoalTableViewCell.swift
+//  PdcaCycler
+//
+//  Created by sel on 2017/05/17.
+//  Copyright © 2017年 sel. All rights reserved.
+//
+
+import UIKit
+
+class GoalTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
